df17b9a843aaeebbdc43ec9ed6a986827fdef116
Allen Sarmiento <allenbsarmiento01@gmail.com>
2022-01-12 16:09:51 -0500

    Update Pneumatics.java

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 public abstract class Pneumatics
 {
@@ -9,8 +12,16 @@ public abstract class Pneumatics
         private static final int COMPRESSOR_ID = 6;
     }
 
+    private static final Compressor compressor = new Compressor(k.COMPRESSOR_ID, PneumaticsModuleType.CTREPCM);
+    private static final DoubleSolenoid doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+
     public static void init()
     {
+        compressor.enableDigital();
+        doubleSolenoid.set(Value.kReverse);
+    }
 
+    public static void toggleSolenoids() {
+        doubleSolenoid.toggle();
     }
 }
