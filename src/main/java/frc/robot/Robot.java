bf46c52051692e3a9894fba6029742ec61db8bfc
Allen Sarmiento <allenbsarmiento01@gmail.com>
2022-02-24 12:34:55 -0500

    Conveyor Control

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Hanger;
@@ -24,12 +25,13 @@ public class Robot extends TimedRobot
         private static final int A = 1, B = 2, X = 3, Y = 4, LB = 5, RB = 6,
             BACK = 7, START = 8, L_STICK = 9, R_STICK = 10;
         private static final int UP = 0, RIGHT = 90, DOWN = 180, LEFT = 270;
-        private static final int CONTROLLER_ID = 0;
+        private static final int CONTROLLER1_ID = 0, CONTROLLER2_ID = 1;
         private static final int PIGEON_ID = 0;
         private static final int PCM_ID = 0; // default node ID
     }
 
-    private final Joystick controller = new Joystick(k.CONTROLLER_ID);
+    private final Joystick controller1 = new Joystick(k.CONTROLLER1_ID);
+    private final Joystick controller2 = new Joystick(k.CONTROLLER2_ID);
     private final PigeonIMU pigeon = new PigeonIMU(k.PIGEON_ID);
     private static final Compressor pcmCompressor = new Compressor(k.PCM_ID, PneumaticsModuleType.CTREPCM);
     private final Timer auto_timer = new Timer();
@@ -75,41 +77,50 @@ public class Robot extends TimedRobot
     public void teleopPeriodic()
     {
         // ==== Drive control ==== //
-        if (controller.getRawButton(k.RB))  // Slow mode
-            Drivetrain.arcadeDrive(controller.getRawAxis(k.LY_ID)/2, controller.getRawAxis(k.RX_ID)/2);
+        if (controller1.getRawButton(k.RB))  // Slow mode
+            Drivetrain.arcadeDrive(controller1.getRawAxis(k.LY_ID)/2, controller1.getRawAxis(k.RX_ID)/2);
         else
-            Drivetrain.arcadeDrive(controller.getRawAxis(k.LY_ID), controller.getRawAxis(k.RX_ID));
+            Drivetrain.arcadeDrive(controller1.getRawAxis(k.LY_ID), controller1.getRawAxis(k.RX_ID));
 
         // ==== Intake ==== //
 
         // Raise/Lower
-        if (controller.getRawButtonPressed(k.X) && Intake._Position == Intake.Position.UP)
+        if (controller1.getRawButtonPressed(k.X) && Intake._Position == Intake.Position.UP)
             Intake.lower();
-        else if (controller.getRawButtonPressed(k.X))
+        else if (controller1.getRawButtonPressed(k.X))
             Intake.raise();
 
         // Spin
-        if (controller.getRawButtonPressed(k.A) && Intake._RunState != Intake.RunState.FORWARD)
+        if (controller1.getRawButtonPressed(k.A) && Intake._RunState != Intake.RunState.FORWARD)
             Intake.forward();
-        else if (controller.getRawButtonPressed(k.B) && Intake._RunState != Intake.RunState.REVERSE)
+        else if (controller1.getRawButtonPressed(k.B) && Intake._RunState != Intake.RunState.REVERSE)
             Intake.reverse();
-        else if (controller.getRawButtonPressed(k.A) || controller.getRawButtonPressed(k.B))
+        else if (controller1.getRawButtonPressed(k.A) || controller1.getRawButtonPressed(k.B))
             Intake.stop();
+
+        // ==== Conveyor ==== //
+
+        if (controller2.getRawButtonPressed(k.A) && Conveyor._RunState != Conveyor.RunState.FORWARD)
+            Conveyor.forward();
+        else if (controller2.getRawButtonPressed(k.B) && Conveyor._RunState != Conveyor.RunState.REVERSE)
+            Conveyor.reverse();
+        else if (controller2.getRawButtonPressed(k.A) || controller2.getRawButtonPressed(k.B))
+            Conveyor.stop();
 
         // === Hanger === //
 
         // Vertical
-        if (controller.getRawButton(k.UP))
+        if (controller2.getRawButton(k.UP))
             Hanger.raise();
-        else if (controller.getRawButton(k.DOWN))
+        else if (controller2.getRawButton(k.DOWN))
             Hanger.lower();
         else
             Hanger.stop();
 
         // Angle
-        if (controller.getRawButtonPressed(k.RIGHT) && Hanger._Angle != Hanger.Angle.FORWARD)
+        if (controller2.getRawButtonPressed(k.RIGHT) && Hanger._Angle != Hanger.Angle.FORWARD)
             Hanger.forward();
-        else if (controller.getRawButtonPressed(k.LEFT) && Hanger._Angle != Hanger.Angle.REST)
+        else if (controller2.getRawButtonPressed(k.LEFT) && Hanger._Angle != Hanger.Angle.REST)
             Hanger.rest();
 
         // ==== Pigeon ==== //
@@ -121,6 +132,7 @@ public class Robot extends TimedRobot
             absHeading += 360;
 
         // ==== Telemetry ==== //
+        
         Drivetrain.printEncoderCount();
         SmartDashboard.putNumber("Raw Heading", rawHeading);
         SmartDashboard.putNumber("Abs Heading", absHeading);
